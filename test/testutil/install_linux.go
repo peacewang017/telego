625bea84911a1cd9707b38bbd15f4f47ced6eb18
ActivePeter <1020401660@qq.com>
2025-05-06 05:40:42 -0700

    install fail should be ok

--- test/testutil/install_linux.go
+++ test/testutil/install_linux.go
@@ -15,36 +15,40 @@ type LinuxInstall struct {
 }
 
 func (l *LinuxInstall) Install(t *testing.T) {
-	cmds := []string{
-		"apt-get install -y " + l.DefaultAppName,
-		"dnf install -y " + l.DefaultAppName,
-		"yum install -y " + l.DefaultAppName,
-		"apk add -y " + l.DefaultAppName,
-		"brew install -y " + l.DefaultAppName,
-	}
+	// Define commands as arrays of strings
+	aptCmd := []string{"apt-get", "install", "-y", l.DefaultAppName}
+	dnfCmd := []string{"dnf", "install", "-y", l.DefaultAppName}
+	yumCmd := []string{"yum", "install", "-y", l.DefaultAppName}
+	apkCmd := []string{"apk", "add", l.DefaultAppName}       // Note: apk doesn't use -y flag
+	brewCmd := []string{"brew", "install", l.DefaultAppName} // Note: brew doesn't need -y flag
 
+	// Override with specific package names if provided
 	if l.SpecAptName != "" {
-		cmds[0] = "apt-get install -y " + l.SpecAptName
+		aptCmd[3] = l.SpecAptName
 	}
 	if l.SpecDnfName != "" {
-		cmds[1] = "dnf install -y " + l.SpecDnfName
+		dnfCmd[3] = l.SpecDnfName
 	}
 	if l.SpecYumName != "" {
-		cmds[2] = "yum install -y " + l.SpecYumName
+		yumCmd[3] = l.SpecYumName
 	}
 	if l.SpecApkName != "" {
-		cmds[3] = "apk add -y " + l.SpecApkName
+		apkCmd[2] = l.SpecApkName
 	}
 	if l.SpecBrewName != "" {
-		cmds[4] = "brew install -y " + l.SpecBrewName
+		brewCmd[2] = l.SpecBrewName
 	}
 
-	for _, cmd := range cmds {
-		if err := RunCommand(t, exec.Command(cmd)); err != nil {
-			t.Fatalf("安装 %s 失败: %v", l.DefaultAppName, err)
+	// List of command arrays to try
+	commands := [][]string{aptCmd, dnfCmd, yumCmd, apkCmd, brewCmd}
+
+	for _, cmdArgs := range commands {
+		if err := RunCommand(t, exec.Command(cmdArgs[0], cmdArgs[1:]...)); err != nil {
+			t.Logf("安装 %s 失败: %v", l.DefaultAppName, err)
 		} else {
 			t.Logf("安装 %s 成功", l.DefaultAppName)
 			return
 		}
 	}
+	t.Fatalf("已经尝试若干安装方式，安装 %s 失败", l.DefaultAppName)
 }
